77fdaf85f987d25ff03d16510dac1b76ba94ef73
Hafeez-ahad <itxhafeez252@gmail.com>
2024-04-26 23:15:04 +0500

    Add files via upload

---
+++ assignment 07/aap.js
@@ -0,0 +1,98 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+// 3. Declare and initialize a strings array.
+// 4. Declare and initialize a numbers array.
+// 5. Declare and initialize a boolean array.
+// 6. Declare and initialize a mixed array.
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+
+
+// ans no 1 
+//  let arry = []
+
+
+
+//  ans no 2
+
+//  arry = ['string array'];
+//  console.log (arry)
+
+
+
+//  ans no 3
+// numberArry = [25];
+// console.log (numberArry)
+
+// ans no 4
+// boleanarry = [true]
+// console.log (boleanarry)
+
+// ans no 5
+// mixedarry = ['school',5]
+// console.log(mixedarry)
+
+// ans no 7// 
+
+
+// Declare and initialize an array with education qualifications
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// Displaying the qualifications in the browser with numbers
+// document.write("<h2>Educational Qualifications:</h2>");
+// document.write("<ol>");
+// for (var i = 0; i < qualifications.length; i++) {
+//   document.write("<li>" + qualifications[i] + "</li>");
+// }
+// document.write("</ol>");
+
+// let arryOf7 = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write("<h2>Qualification</h2>");
+// document.write ("<ul>");
+// document.write ("<ol>");
+//  for (i=0; i<=arryOf7.length-1; i++){
+//   document.write ("<li>", arryOf7[i], "</li>")
+//  }
+//  document.write ("</ol>");
+//  document.write ("</ul>")
+
+
+// ***********************************************************************
+
+
+// // assinment no 8
+//  ans no 1 declare and initilize multidimensional array
+
+// let mulArry = [
+    
+// ]
+// console.log (mulArry)
+
+// // an no 2
+
+// let mulArray2 = [
+//     [1,2,3],
+//     [8,9,10],
+//     [5,88,99]
+// ]
+
+// console.log (mulArray2)
+
+
+// // no 3 array counting from 1 to 100
+ 
+// let array = [9];
+// let i=0
+// while (i <= array[0]){
+//     i++
+//     console.log(`using while `,i)
+// }
+
+// for (i=1; i<array[0]; i++){
+//     console.log(`using for loop `,i)
+// }
